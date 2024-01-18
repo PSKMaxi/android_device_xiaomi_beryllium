f9b697e5406744173a1b1c4d1a3775304bc6b8e8
Legendleo90 <akshatleo9456@gmail.com>
2024-01-18 10:34:23 +0530

    beryllium: Superior-ify

--- superior_beryllium.mk
+++ superior_beryllium.mk
@@ -1,19 +1,32 @@
 #
 # Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from beryllium device
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# SuperiorOS stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := false
+TARGET_GAPPS_ARCH := arm64
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_USES_MIUI_CAMERA := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := superior_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := POCO F1
+PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := beryllium
