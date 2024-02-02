ecba3fe9aeb56179dc3a41c1975b1f44722c9378
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2024-02-02 19:13:38 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blackiron_beryllium.mk
+    $(LOCAL_DIR)/derp_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    blackiron_beryllium-user \
-    blackiron_beryllium-userdebug \
-    blackiron_beryllium-eng
+    derp_beryllium-user \
+    derp_beryllium-userdebug \
+    derp_beryllium-eng
