05f5a54117ce0bfd140502102c343cce7d19ed9f
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2024-02-04 14:45:08 +0530

    Update lineage_beryllium.mk

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common project matrixx stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
