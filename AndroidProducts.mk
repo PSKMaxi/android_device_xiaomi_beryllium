edd33a48bbf21e2e4897ff91d39c53cf03fb945f
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2024-02-08 15:38:25 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/genesis_beryllium.mk
+    $(LOCAL_DIR)/lineage_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    genesis_beryllium-user \
-    genesis_beryllium-userdebug \
-    genesis_beryllium-eng
+    lineage_beryllium-user \
+    lineage_beryllium-userdebug \
+    lineage_beryllium-eng
