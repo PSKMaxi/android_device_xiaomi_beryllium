ba07a660001e67883807c29a47204c10be9a2d8c
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2024-02-13 12:01:49 +0530

    Update and rename superior_beryllium.mk to voltage_beryllium.mk

--- voltage_beryllium.mk
+++ voltage_beryllium.mk
@@ -7,8 +7,8 @@
 # Inherit from beryllium device.
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common superior stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common voltage os stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -39,15 +39,13 @@ TARGET_SUPPORTS_BLUR := false
 USE_DUCKDUCKGO := false
 USE_ViaBrowser := false
 TARGET_BUILD_VIMUSIC := false
-USE_MOTO_CALCULATOR := true
-LINKTOWINDOWS_PACKAGES=true
 
 # Build details
-SUPERIOR_MAINTAINER := Max
-SUPERIOR_BUILD_TYPE := Unofficial
+VOLTAGE_MAINTAINER := Max
+VOLTAGE_BUILD_TYPE := Unofficial
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_beryllium
+PRODUCT_NAME := voltage_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
